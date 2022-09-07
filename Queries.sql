55bd91a4a5da7204eb6dd0acace34c47e5206620
Yameena Khan <yameena.adnan@yahoo.com>
2022-09-07 16:33:05 -0500

    Adding commented queries file

--- Queries.sql
+++ Queries.sql
@@ -2,7 +2,7 @@
 
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
 
-select e.emp_no,e.first_name,e.last_name,e.sex, s.salary 
+select e.emp_no,e.last_name, e.first_name,e.sex, s.salary 
 from employees e
   join salaries s
   on (e.emp_no = s.emp_no);
@@ -18,7 +18,7 @@ where
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
-select m.dept_no,m.emp_no,e.first_name,e.last_name,d.dept_name
+select m.dept_no,m.emp_no,e.last_name,e.first_name,d.dept_name
 from dept_manager m
  join employees e 
  on (m.emp_no=e.emp_no)
@@ -28,11 +28,11 @@ from dept_manager m
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
 
-select de.emp_no, e.first_name,e.last_name, d.dept_name
+select de.emp_no,e.last_name, e.first_name, d.dept_name
 from dept_emp de
-inner join employees e
+join employees e
 	on (de.emp_no= e.emp_no)
-inner join departments d
+join departments d
 	on (de.dept_no= d.dept_no);	
 
  
